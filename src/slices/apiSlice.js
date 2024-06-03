21695c47b993de72bfbe213568c8a5b93e73ff19
AdhilKappil <adhilalikappil@gmail.com>
2024-06-03 13:08:09 +0530

    update in vite config

--- src/slices/apiSlice.js
+++ src/slices/apiSlice.js
@@ -1,9 +1,12 @@
 import {createApi, fetchBaseQuery} from '@reduxjs/toolkit/query/react'
 
-const baseQuery = fetchBaseQuery({baseUrl:''});
+const baseQuery = fetchBaseQuery({baseUrl:'https://vocalizemanager-backend.onrender.com'});
 
 export const apiSlice = createApi({
     baseQuery,
     tagTypes: ['User'],
     endpoints:(builder) => ({})
 })
+
+import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
+
--- vite.config.js
+++ vite.config.js
@@ -1,22 +1,6 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
-import dotenv from 'dotenv';
-
-dotenv.config();
-
-// Retrieve environment variables
-const { BASE_URL } = process.env;
 
 export default defineConfig({
   plugins: [react()],
-  server: {
-    port: 8000,
-    proxy: {
-      '/api': {
-        target: BASE_URL,
-        changeOrigin: true,
-        rewrite: (path) => path.replace(/^\/api/, ''),
-      },
-    },
-  },
 });
